64bfffbb260fda037dcc6c3e02794e46dd488827
Deleon09 <54414721+Deleon09@users.noreply.github.com>
2020-06-02 15:10:50 -0600

    ADD: scores modification

--- Arkanoid/IArkanoid/Scores.Designer.cs
+++ Arkanoid/IArkanoid/Scores.Designer.cs
@@ -31,6 +31,12 @@ namespace IArkanoid
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 =
+                new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 =
+                new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 =
+                new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvScores = new System.Windows.Forms.DataGridView();
             this.btnRegresar = new System.Windows.Forms.Button();
@@ -54,7 +60,7 @@ namespace IArkanoid
                 ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))),
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(211, 24);
+            this.label1.Location = new System.Drawing.Point(216, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(423, 90);
             this.label1.TabIndex = 0;
@@ -63,11 +69,46 @@ namespace IArkanoid
             // 
             // dgvScores
             // 
+            this.dgvScores.BackgroundColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))),
+                ((int) (((byte) (0)))), ((int) (((byte) (64)))));
+            this.dgvScores.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvScores.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Turquoise;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvScores.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvScores.ColumnHeadersHeightSizeMode =
                 System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvScores.Location = new System.Drawing.Point(280, 114);
+            this.dgvScores.EnableHeadersVisualStyles = false;
+            this.dgvScores.GridColor = System.Drawing.Color.Cyan;
+            this.dgvScores.Location = new System.Drawing.Point(296, 167);
             this.dgvScores.Name = "dgvScores";
-            this.dgvScores.Size = new System.Drawing.Size(285, 422);
+            this.dgvScores.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))),
+                ((int) (((byte) (0)))), ((int) (((byte) (64)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvScores.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvScores.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))),
+                ((int) (((byte) (0)))), ((int) (((byte) (64)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Impact", 8.25F,
+                ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))),
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            this.dgvScores.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvScores.Size = new System.Drawing.Size(285, 335);
             this.dgvScores.TabIndex = 1;
             // 
             // btnRegresar
@@ -76,7 +117,8 @@ namespace IArkanoid
                 ((int) (((byte) (192)))));
             this.btnRegresar.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnRegresar.Location = new System.Drawing.Point(285, 564);
+            this.btnRegresar.ForeColor = System.Drawing.Color.White;
+            this.btnRegresar.Location = new System.Drawing.Point(287, 532);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(280, 60);
             this.btnRegresar.TabIndex = 2;
@@ -89,7 +131,7 @@ namespace IArkanoid
             this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(229, 114);
+            this.label2.Location = new System.Drawing.Point(246, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 22);
             this.label2.TabIndex = 3;
@@ -101,7 +143,7 @@ namespace IArkanoid
             this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(237, 136);
+            this.label3.Location = new System.Drawing.Point(254, 189);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 27);
             this.label3.TabIndex = 4;
@@ -113,7 +155,7 @@ namespace IArkanoid
             this.label4.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(237, 163);
+            this.label4.Location = new System.Drawing.Point(254, 216);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 27);
             this.label4.TabIndex = 5;
@@ -125,7 +167,7 @@ namespace IArkanoid
             this.label5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(237, 190);
+            this.label5.Location = new System.Drawing.Point(254, 242);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(37, 27);
             this.label5.TabIndex = 6;
@@ -137,7 +179,7 @@ namespace IArkanoid
             this.label6.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(237, 378);
+            this.label6.Location = new System.Drawing.Point(254, 432);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 27);
             this.label6.TabIndex = 7;
@@ -149,7 +191,7 @@ namespace IArkanoid
             this.label7.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(237, 352);
+            this.label7.Location = new System.Drawing.Point(254, 404);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(37, 27);
             this.label7.TabIndex = 8;
@@ -161,7 +203,7 @@ namespace IArkanoid
             this.label8.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(237, 217);
+            this.label8.Location = new System.Drawing.Point(254, 269);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(37, 27);
             this.label8.TabIndex = 9;
@@ -173,7 +215,7 @@ namespace IArkanoid
             this.label9.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(237, 271);
+            this.label9.Location = new System.Drawing.Point(254, 324);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(37, 27);
             this.label9.TabIndex = 10;
@@ -185,7 +227,7 @@ namespace IArkanoid
             this.label10.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(237, 243);
+            this.label10.Location = new System.Drawing.Point(254, 297);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(37, 27);
             this.label10.TabIndex = 11;
@@ -197,7 +239,7 @@ namespace IArkanoid
             this.label11.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(237, 325);
+            this.label11.Location = new System.Drawing.Point(254, 378);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(37, 27);
             this.label11.TabIndex = 12;
@@ -209,7 +251,7 @@ namespace IArkanoid
             this.label12.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(237, 298);
+            this.label12.Location = new System.Drawing.Point(254, 351);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(37, 27);
             this.label12.TabIndex = 13;
