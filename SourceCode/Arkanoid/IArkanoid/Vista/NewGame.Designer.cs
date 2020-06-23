ae30afd87e597e9a3d70c268320a52001650e97d
Guille Climaco <00001318@uca.edu.sv>
2020-06-22 18:29:46 -0600

    Fix: Image bug

--- SourceCode/Arkanoid/IArkanoid/Vista/NewGame.Designer.cs
+++ SourceCode/Arkanoid/IArkanoid/Vista/NewGame.Designer.cs
@@ -32,7 +32,8 @@ namespace IArkanoid
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewGame));
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(NewGame));
             this.lblLives = new System.Windows.Forms.Label();
             this.picLogo = new System.Windows.Forms.PictureBox();
             this.lblScore = new System.Windows.Forms.Label();
@@ -48,40 +49,52 @@ namespace IArkanoid
             // 
             // lblLives
             // 
-            this.lblLives.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))), ((int) (((byte) (64)))));
-            this.lblLives.Font = new System.Drawing.Font("Impact", 18F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblLives.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))),
+                ((int) (((byte) (64)))));
+            this.lblLives.Font = new System.Drawing.Font("Impact", 18F,
+                ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))),
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lblLives.ForeColor = System.Drawing.Color.White;
             this.lblLives.Location = new System.Drawing.Point(-1, 0);
             this.lblLives.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLives.Name = "lblLives";
-            this.lblLives.Size = new System.Drawing.Size(212, 49);
+            this.lblLives.Size = new System.Drawing.Size(247, 57);
             this.lblLives.TabIndex = 0;
             this.lblLives.Text = "LIVES:";
             this.lblLives.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // picLogo
             // 
-            this.picLogo.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
-            this.picLogo.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))), ((int) (((byte) (64)))));
+            this.picLogo.Anchor =
+                ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top |
+                                                        System.Windows.Forms.AnchorStyles.Left) |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.picLogo.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))),
+                ((int) (((byte) (64)))));
             this.picLogo.Image = ((System.Drawing.Image) (resources.GetObject("picLogo.Image")));
-            this.picLogo.Location = new System.Drawing.Point(206, 0);
+            this.picLogo.Location = new System.Drawing.Point(240, 0);
             this.picLogo.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(332, 49);
+            this.picLogo.Size = new System.Drawing.Size(387, 57);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picLogo.TabIndex = 1;
             this.picLogo.TabStop = false;
             // 
             // lblScore
             // 
-            this.lblScore.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblScore.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))), ((int) (((byte) (64)))));
-            this.lblScore.Font = new System.Drawing.Font("Impact", 18F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblScore.Anchor =
+                ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.lblScore.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))),
+                ((int) (((byte) (64)))));
+            this.lblScore.Font = new System.Drawing.Font("Impact", 18F,
+                ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))),
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lblScore.ForeColor = System.Drawing.Color.White;
-            this.lblScore.Location = new System.Drawing.Point(536, 0);
+            this.lblScore.Location = new System.Drawing.Point(625, 0);
             this.lblScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(204, 49);
+            this.lblScore.Size = new System.Drawing.Size(238, 57);
             this.lblScore.TabIndex = 2;
             this.lblScore.Text = "SCORE:";
             this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -89,10 +102,11 @@ namespace IArkanoid
             // heart3
             // 
             this.heart3.BackColor = System.Drawing.Color.Transparent;
-            this.heart3.Location = new System.Drawing.Point(68, 156);
+            this.heart3.Image = ((System.Drawing.Image) (resources.GetObject("heart3.Image")));
+            this.heart3.Location = new System.Drawing.Point(79, 180);
             this.heart3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.heart3.Name = "heart3";
-            this.heart3.Size = new System.Drawing.Size(29, 29);
+            this.heart3.Size = new System.Drawing.Size(34, 33);
             this.heart3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.heart3.TabIndex = 5;
             this.heart3.TabStop = false;
@@ -100,10 +114,11 @@ namespace IArkanoid
             // heart2
             // 
             this.heart2.BackColor = System.Drawing.Color.Transparent;
-            this.heart2.Location = new System.Drawing.Point(34, 156);
+            this.heart2.Image = ((System.Drawing.Image) (resources.GetObject("heart2.Image")));
+            this.heart2.Location = new System.Drawing.Point(40, 180);
             this.heart2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.heart2.Name = "heart2";
-            this.heart2.Size = new System.Drawing.Size(29, 29);
+            this.heart2.Size = new System.Drawing.Size(34, 33);
             this.heart2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.heart2.TabIndex = 6;
             this.heart2.TabStop = false;
@@ -111,10 +126,11 @@ namespace IArkanoid
             // heart1
             // 
             this.heart1.BackColor = System.Drawing.Color.Transparent;
-            this.heart1.Location = new System.Drawing.Point(0, 156);
+            this.heart1.Image = ((System.Drawing.Image) (resources.GetObject("heart1.Image")));
+            this.heart1.Location = new System.Drawing.Point(0, 180);
             this.heart1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.heart1.Name = "heart1";
-            this.heart1.Size = new System.Drawing.Size(29, 29);
+            this.heart1.Size = new System.Drawing.Size(34, 33);
             this.heart1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.heart1.TabIndex = 7;
             this.heart1.TabStop = false;
@@ -127,12 +143,12 @@ namespace IArkanoid
             // 
             // NewGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(739, 442);
+            this.ClientSize = new System.Drawing.Size(862, 510);
             this.Controls.Add(this.heart1);
             this.Controls.Add(this.heart2);
             this.Controls.Add(this.heart3);
